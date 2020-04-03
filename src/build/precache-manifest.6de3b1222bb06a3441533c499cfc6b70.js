ab18afd2c4bf4c35f152905c23ddad138d6a10fd
Julian Hamm <julianhamm1@gmail.com>
2020-04-03 17:47:08 +0100

    First commit

---
+++ src/build/precache-manifest.6de3b1222bb06a3441533c499cfc6b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f145aec8268b79b612441effd4df1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5707213d51d315cac293",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "b0ad3394f6f42c8d9bec",
+    "url": "/static/js/2.d57d68f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d57d68f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5707213d51d315cac293",
+    "url": "/static/js/main.1307422f.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
